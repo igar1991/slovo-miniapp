f80043b533caf5c8af1750eb81ab65df213a0bcf
igar1991 <iiggaarr19912004@gmail.com>
2020-05-05 22:42:04 +0300

    addLike

--- src/App.js
+++ src/App.js
@@ -19,19 +19,10 @@ const App = () => {
 	const [activeStory, setActiveStory] = useState('word_day');
 	const [activePanel, setActivePanel] = useState('top_like');
 	const [fetchedUser, setUser] = useState(null);
-	const [popout, setPopout] = useState(<Alert
-        actionsLayout="vertical"
-        actions={[{
-          title: 'Превосходно!',
-          autoclose: true,
-          mode: 'cancel'
-        }]}
-        onClose={()=>setPopout(null)}
-      >
-        <h2>Слово дня</h2>
-        <p>Существует много слов, которые мы часто слышим, но не знаем их точных значений. Каждый день узнавайте точные значения слов русского языка!</p>
-      </Alert>);
+	const [updatePopout, setUpdatePopout] = useState(<ScreenSpinner/>)
+	const [popout, setPopout] = useState(null);
 	const [itemId, setItemId] = useState(null);
+	const [notifications, setNotifications] = useState(false);
 
 	useEffect(() => {
 		bridge.subscribe(({ detail: { type, data }}) => {
@@ -43,9 +34,35 @@ const App = () => {
 		});
 		async function fetchData() {
 			const user = await bridge.send('VKWebAppGetUserInfo');
+			bridge.send("VKWebAppStorageGet", {"keys": ["check"]}).then(data=>{
+				console.log(data)
+				if(data.keys[0].value==='1') {
+					setPopout(null)
+				} else {
+				setPopout(<Alert
+					actionsLayout="vertical"
+					actions={[{
+					  title: 'Превосходно!',
+					  autoclose: true,
+					  mode: 'cancel'
+					}]}
+					onClose={()=>{
+						setPopout(null)
+						bridge.send("VKWebAppStorageSet", {"key": "check", "value": "1"})
+					}}
+				  >
+					<h2>Слово дня</h2>
+					<p>Существует много слов, которые мы часто слышим, но не знаем их точных значений. Каждый день узнавайте точные значения слов русского языка!</p>
+				  </Alert>)
+				}
+				
+			})
 			setUser(user);
+			setUpdatePopout(null)
+			
 		}
 		fetchData();
+
 	}, []);
 
 	const go = e => {
@@ -62,6 +79,37 @@ const App = () => {
 
 	const itemWord = DATA_TOP.find(item=>itemId===item.id);
 
+	const allowNtifications=()=>{
+		bridge.send("VKWebAppAllowNotifications", {})
+		.then(data=> {
+			setUpdatePopout(<ScreenSpinner/>)
+			setNotifications(data.result)
+			setUpdatePopout(null)
+		             }
+			)
+		.catch(error=>setPopout(<Alert
+			actions={[{
+			  title: 'Закрыть',
+			  autoclose: true,
+			  mode: 'cancel'
+			}]}
+			onClose={()=>setPopout(null)}
+		  >
+			<h2>Упс...</h2>
+			<p>Нам нужно ваше разрешение на отправку уведомлений</p>
+		  </Alert>))
+	}
+
+	const denyNtifications=()=> {
+		bridge.send("VKWebAppDenyNotifications", {})
+		.then(data=>{
+			setUpdatePopout(<ScreenSpinner/>)
+			setNotifications(!data.result)
+			setUpdatePopout(null)
+		             })
+	}
+
+
 	return (
 		<Epic activeStory={activeStory} tabbar={
 			<Tabbar>
@@ -77,13 +125,13 @@ const App = () => {
 			  ><Icon28ListLikeOutline /></TabbarItem>
 			</Tabbar>
 	}>
-		    <View id="word_day" activePanel="word_day" popout={popout}>
+		    <View id="word_day" activePanel="word_day" popout={updatePopout===null?popout:updatePopout}>
 			  <Panel id="word_day">
 			     <PanelHeader>Слово дня</PanelHeader>
-		         <WordInfo data = {DATA_DAY} />
+		         <WordInfo data = {DATA_DAY} notifications={notifications} allowNtifications={allowNtifications} denyNtifications={denyNtifications} />
 			  </Panel> 
 			</View>
-			<View id="top_like" activePanel={activePanel}>
+			<View id="top_like" activePanel={activePanel} popout={updatePopout===null?popout:updatePopout}>
 			  <Panel id="top_like">
 				<PanelHeader>Топ 100 слов</PanelHeader>
 				  <TopList datatop = {DATA_TOP} goPanel={goPanel} />
@@ -92,7 +140,7 @@ const App = () => {
 				<PanelHeader left={<PanelHeaderButton onClick={goBack} data-to="top_like">
 				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
 			</PanelHeaderButton>}>Топ 100 слов</PanelHeader>
-				{itemWord !== null && <WordInfo data = {itemWord} />}
+				{itemWord !== null && <WordInfo data = {itemWord} notifications={notifications} allowNtifications={allowNtifications} denyNtifications={denyNtifications} />}
 			  </Panel>
 			</View>
 	</Epic>
--- src/panels/WordInfo.js
+++ src/panels/WordInfo.js
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {View, Epic, Tabbar, TabbarItem, Panel, PanelHeader, Button, Div } from '@vkontakte/vkui';
-import Icon28LikeOutline from '@vkontakte/icons/dist/28/like_outline';
+import Icon24LikeOutline from '@vkontakte/icons/dist/24/like_outline';
+import Icon24Like from '@vkontakte/icons/dist/24/like';
 import Icon28Notifications from '@vkontakte/icons/dist/28/notifications';
+import Icon28NotificationDisableOutline from '@vkontakte/icons/dist/28/notification_disable_outline';
 import Icon24ShareOutline from '@vkontakte/icons/dist/24/share_outline';
 import './style.css';
+import bridge from '@vkontakte/vk-bridge';
 
-export const WordInfo = ({ data })=> {
+export const WordInfo = ({ data, notifications, allowNtifications, denyNtifications })=> {
+
+    const [like, setLike] = useState(data.is_likes);
+    const [count, setCount] = useState(data.like);
+
+    const shareButton=()=> {
+        bridge.send("VKWebAppShowWallPostBox", {"message": `#СловоДня
+        ${data.word}. 
+        ${data.value} 
+        Узнавай точные значения слов каждый день.👇🏻
+        https://vk.com/app7442230`});
+    }
+
+    const updateLike=()=> {
+        setLike(!like);
+        if(like) {
+            setCount(count-1)
+            bridge.send("VKWebAppStorageSet", {"key": "count", "value": "1"});
+        } else {
+            setCount(count+1)
+        }
+    }
+    
+ 
+
     return (
             <Div className='cardWord'>
 				<h1>{data.word}</h1>
 				<h4>{data.part}</h4>
 				<h3>{data.value}</h3>
 				<span>{data.example}</span>
-				<div className='iconme'><Icon28LikeOutline /><p className="count">{data.like}</p></div>
-                  <Button size="xl" stretched mode="secondary" style={{ marginBottom: 8 }} after={<Icon28Notifications width={20} />}>Получать слова</Button>
-                  <Button size="xl" stretched mode="commerce" after={<Icon24ShareOutline width={20} />} >Поделиться</Button>
+                <div className='iconme'>{
+                like
+                ?
+                <Icon24Like onClick={updateLike} />
+                :
+                <Icon24LikeOutline onClick={updateLike} />
+                }
+                <p className="count">{count}</p>
+                </div>
+                {!notifications
+                ?
+                <Button onClick={allowNtifications} size="xl" stretched mode="secondary" style={{ marginBottom: 8 }} after={<Icon28Notifications width={20} />}>Получать слова</Button>
+                :
+                <Button onClick={denyNtifications} size="xl" stretched mode="destructive" style={{ marginBottom: 8 }} after={<Icon28NotificationDisableOutline width={20} />}>Отключить уведомления</Button>
+                }
+                <Button onClick={shareButton} size="xl" stretched mode="commerce" after={<Icon24ShareOutline width={20} />} >Поделиться</Button>
             </Div>    
 		
     )
